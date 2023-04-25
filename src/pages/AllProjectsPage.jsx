8da45df3b9785649df3aa0aa3fca8b9b51b4a010
Yessy Lee <107485501+YessyLee@users.noreply.github.com>
2023-04-25 19:19:33 +0800

    Fixed some mobile responsiveness

--- src/pages/AllProjectsPage.jsx
+++ src/pages/AllProjectsPage.jsx
@@ -12,7 +12,7 @@ import Slider from "../components/Slider/Slider";
 function AllProjectsPage() {
   // State
   const [projectList, setProjectList] = useState([]);
-  const [shuffledSortedProjectList, setShuffledSortedProjectList] = useState([]);
+  const [SortedProjectList, setSortedProjectList] = useState([]);
 
 
   useEffect(() => {
@@ -41,25 +41,25 @@ function AllProjectsPage() {
   // new - old
   const orderProjectList = () => {
     const orderedList = [...projectList].sort((a, b) => new Date(b.date_created) - new Date(a.date_created));
-    setShuffledSortedProjectList(orderedList);
+    setSortedProjectList(orderedList);
   };
 
   // old - new
   const reorderProjectList = () => {
     const orderedList = [...projectList].sort((a, b) => new Date(a.date_created) - new Date(b.date_created));
-    setShuffledSortedProjectList(orderedList);
+    setSortedProjectList(orderedList);
   };
 
   // a - z
   const sortProjectList = () => {
     const sortedList = [...projectList].sort((a, b) => a.title.localeCompare(b.title));
-    setShuffledSortedProjectList(sortedList);
+    setSortedProjectList(sortedList);
   };
 
   // z - a
   const resortProjectList = () => {
     const sortedList = [...projectList].sort((a, b) => b.title.localeCompare(a.title));
-    setShuffledSortedProjectList(sortedList);
+    setSortedProjectList(sortedList);
   };
 
   //rendering alphabets first
@@ -82,30 +82,30 @@ function AllProjectsPage() {
     <div className='max-w-[1240px] mx-auto text-white relative'>
       <div className='px-6 py-2 md:py-6 lg:py-12'>
           <div className="rounded-lg m-2">
-          <h2 className="text-4xl text-center font-bold pt-10 mt-10">Support your fellow FutureProofME Campaigners</h2>
-          <p className="text-slate-100 p-2 ml-10 mr-10 text-lg text-center">FutureProofME is a crowdfunding platform to help you and your peers raise funds for education. FutureProofME can help you overcome financial barriers that may otherwise prevent you from pursuing your desired career. By using FutureProofME, you can create campaigns to raise funds for your educational expenses, including tuition fees, exam certification, textbooks, and other course materials.</p>
+          <h2 className="lg:text-4xl md:text-3xl text-2xl text-center font-bold pt-10 mt-10">Support your fellow FutureProofME Campaigners</h2>
+          <p className="text-slate-100 p-2 ml-10 mr-10 lg:text-lg md:text-md text-sm text-center">FutureProofME is a crowdfunding platform to help you and your peers raise funds for education. FutureProofME can help you overcome financial barriers that may otherwise prevent you from pursuing your desired career. By using FutureProofME, you can create campaigns to raise funds for your educational expenses, including tuition fees, exam certification, textbooks, and other course materials.</p>
 
-          <p className='text-white-100 font-bold text-lg text-center'><button type="button" 
-            className="text-sm font-bold p-3 m-3 rounded-lg text-slate-100 text-center bg-gradient-to-r from-green-400 to-blue-500 hover:from-indigo-500 hover:to-yellow-500 
+          <p className='text-white-100 font-bold lg:text-lg md:text-md text-sm text-center'><button type="button" 
+            className="text-sm font-bold pl-2 pr-2 m-1 lg:p-3 lg:m-3 rounded-lg text-slate-100 text-center bg-gradient-to-r from-green-400 to-blue-500 hover:from-indigo-500 hover:to-yellow-500 
             sm:text-lg "><Link to="/registration">
             SIGN UP NOW</Link></button>to create your campaign or to donate. Now it's time to future proof yourself and your peers! 🚀</p>
           </div>
         </div>
 
         <div className="grid lg:grid-cols-3 md:grid-cols-2 pb-16 pl-12 pr-12 gap-7 flex-col">
-          <div className="shadow-xl bg-slate-100 p-8 m-4 rounded-xl">
-            <h2 className="text-indigo-600 text-4xl text-center font-bold">Over $100K</h2>
-            <p className="text-lg text-center font-bold text-slate-500">raised toward upskilling!</p>
+          <div className="shadow-xl bg-slate-200 p-8 m-4 rounded-xl">
+            <h2 className="text-indigo-600 lg:text-4xl md:text-3xl text-2xl text-center font-bold">Over $100K</h2>
+            <p className="lg:text-lg md:text-md text-sm text-center font-bold text-slate-500">raised toward upskilling!</p>
           </div>
 
-          <div className="shadow-xl bg-slate-100 p-8 m-4 rounded-xl">
-            <h2 className="text-indigo-600 text-4xl text-center font-bold">Over 1K Users</h2>
-            <p className="text-lg text-center font-bold text-slate-500">reach their career goal!</p>
+          <div className="shadow-xl bg-slate-200 p-8 m-4 rounded-xl">
+            <h2 className="text-indigo-600 lg:text-4xl md:text-3xl text-2xl text-center font-bold">Over 1K Users</h2>
+            <p className="lg:text-lg md:text-md text-sm text-center font-bold text-slate-500">reach their career goal!</p>
           </div>
 
-          <div className="shadow-xl bg-slate-100 p-8 m-4 rounded-xl">
-            <h2 className="text-indigo-600 text-4xl text-center font-bold">Faster Together</h2>
-            <p className="text-lg text-center font-bold text-slate-500">collectively reach goals faster!</p>
+          <div className="shadow-xl bg-slate-200 p-8 m-4 rounded-xl">
+            <h2 className="text-indigo-600 lg:text-4xl md:text-3xl text-2xl text-center font-bold">Faster Together</h2>
+            <p className="lg:text-lg md:text-md text-sm text-center font-bold text-slate-500">collectively reach goals faster!</p>
           </div>
         </div>
 
@@ -114,10 +114,10 @@ function AllProjectsPage() {
 
         <Menu as="div" className="relative inline-block pb-10 pl-10">
           <div>
-          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-3 text-md font-semibold text-gray-900 
+          <Menu.Button className="inline-flex w-full justify-center gap-x-5 rounded-md bg-white px-3 py-3 text-md font-semibold text-gray-900 
           shadow-sm ring-2 ring-inset ring-indigo-400 hover:bg-indigo-200">
           Sort options
-          <ChevronDownIcon className="-mr-1 h-5 w-5 text-indigo-800" aria-hidden="true" />
+          <ChevronDownIcon className="mr-1 h-5 w-5 text-indigo-800" aria-hidden="true" />
           </Menu.Button>
          </div>
 
@@ -130,13 +130,13 @@ function AllProjectsPage() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
               <button onClick={sortProjectList} className={classNames(
                 active ? 'bg-gray-100 text-indigo-700' : 'text-indigo-500',
-                'block px-4 py-2 text-sm font-bold'
+                'block px-2 py-2 text-sm font-bold'
               )}
               >
               a ➔ z
@@ -148,7 +148,7 @@ function AllProjectsPage() {
               {({ active }) => (
               <button onClick={resortProjectList} className={classNames(
                 active ? 'bg-gray-100 text-indigo-700' : 'text-indigo-500',
-                'block px-4 py-2 text-sm font-bold'
+                'block px-2 py-2 text-sm font-bold'
               )}
               >
               z ➔ a
@@ -160,7 +160,7 @@ function AllProjectsPage() {
               {({ active }) => (
               <button onClick={orderProjectList} className={classNames(
                 active ? 'bg-gray-100 text-indigo-700' : 'text-indigo-500',
-                'block px-4 py-2 text-sm font-bold'
+                'block px-2 py-2 text-sm font-bold'
               )}
               >
               new ➔ old
@@ -172,7 +172,7 @@ function AllProjectsPage() {
               {({ active }) => (
               <button onClick={reorderProjectList} className={classNames(
                 active ? 'bg-gray-100 text-indigo-700' : 'text-indigo-500',
-                'block px-4 py-2 text-sm font-bold'
+                'block px-2 py-2 text-sm font-bold'
               )}
               >
               old ➔ new
@@ -187,18 +187,18 @@ function AllProjectsPage() {
   
 
         <div className="">
-          <div className="grid grid-cols-3 pl-10 pr-10 gap-8">
-            {shuffledSortedProjectList.map((project, key) => {
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 p-10 gap-8 flex-col">
+            {SortedProjectList.map((project, key) => {
               return <ProjectCard key={key} projectData={project} />;
             })}
           </div>
         </div>
 
         <div className="bg-gradient-to-r from-indigo-900 from-10% via-sky-600 via-30% to-indigo-900 to-90% rounded-lg shadow-2xl mt-12 h-[120px] -m-16">
-<div className="flex justify-between">
-<h1 className="text-left text-xl font-bold text-slate-100 pt-12 p-8">Register today & start your very own futureproofME campaign.</h1>
+<div className="flex lg:justify-between ml-10 mr-5">
+<h1 className="text-left lg:text-xl text-sm lg:font-bold font-semibold text-slate-100 lg:pt-12 p-8">Register today & start your very own futureproofME campaign.</h1>
 <p className='ml-10 mr-10 pt-6 text-white-100 text-left'><button type="button" 
-className=" py-2 text-sm font-bold p-8 m-4 rounded-full text-slate-100 text-center bg-pink-500 hover:bg-indigo-400 sm:text-lg "><Link to="/registration">
+className=" py-2 text-sm font-bold p-6 m-4 rounded-full text-slate-100 text-center bg-pink-500 hover:bg-indigo-400 sm:text-lg "><Link to="/registration">
 Get Started!</Link></button></p>
 </div>
 </div>
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -39,37 +39,37 @@ function ProjectPage() {
 
     <body className="flex min-h-screen justify-center items-center">
       <div className="pt-24 pl-36 pr-36 mt-24">
-        <div className="grid grid-cols-2 gap-16">
+        <div className="grid lg:grid-cols-2 md:lg:grid-cols-2 gap-8 pl-4 pr-4 lg:gap-16">
           
           <div>
             <img className="rounded-lg shadow-md w-full h-96 object-cover" src={project.image} />
           </div>
           
           <div className="bg-indigo-50 p-5 rounded-lg shadow-md">
-            <h2 className="font-bold text-indigo-800 text-3xl mb-5">{project.title}</h2>
+            <h2 className="font-bold text-indigo-800 lg:text-3xl text-2xl mb-5">{project.title}</h2>
             
-            <p className="text-slate-500 text-lg">
-            <h3 className="font-bold text-indigo-500 text-xl mb-1">
+            <p className="text-slate-500 text-sm lg:text-lg">
+            <h3 className="font-bold text-indigo-500 lg:text-xl text-lg mb-1">
               Campaign overview:</h3>
               <h3 className="text-slate-500 text-sm font-bold mb-3">Published by {owner.username} on {new Date(project.date_created).toLocaleDateString()}</h3>
               {project.description}</p>
 
-              <p className="font-bold text-indigo-800 text-center text-lg mb-1 mt-10">Campaign Goal</p> 
+              <p className="font-bold text-indigo-800 text-center lg:text-lg mb-1 lg:mt-10 mt-4">Campaign Goal</p> 
               <ProgressBar goal={project.goal} total={project.total} />
           </div>
 
 
           <div className="shadow-lg pt-10 mb-5 rounded-lg">
-            <p className="font-bold text-indigo-500 text-center text-2xl mb-3">{project.is_open ? <p>Status: Accepting Donations</p> : <p>Status: Goal Achieved</p>}</p>
+            <p className="font-bold text-indigo-500 text-center lg:text-2xl text-xl mb-3">{project.is_open ? <p>Status: Accepting Donations</p> : <p>Status: Goal Achieved</p>}</p>
             <PledgeForm project={project} /> 
           </div>
 
           <div className="mb-10 mt-10">
-            <h3 className="font-bold text-indigo-500 text-center text-2xl mb-5">Thank you for your donations!</h3>
+            <h3 className="font-bold text-indigo-500 text-center text-xl lg:text-2xl mb-5">Thank you for your donations!</h3>
             <ul>
              {project.pledges.map((pledgeData, key) => {
               return (
-              <span className= "block h-15 bg-indigo-50 rounded-xl p-5 mb-3 text-slate-500 text-lg" key={key}>
+              <span className= "block h-15 bg-indigo-50 rounded-xl p-5 mb-3 text-slate-500 lg:text-lg" key={key}>
               ${pledgeData.amount} donated by {" "}
               {pledgeData.supporter ? pledgeData.supporter : "Anonymous"}
               <p className="font-bold ">"{pledgeData.comment}"</p>
@@ -83,7 +83,7 @@ function ProjectPage() {
           <div className="bg-gradient-to-r from-slate-900 from-10% via-sky-600 
 via-30% to-indigo-900 to-90% rounded-lg shadow-2xl mt-10">
 <div className="flex justify-between">
-<h1 className="text-left text-xl font-bold text-slate-100 p-6">Register 
+<h1 className="text-left lg:text-xl font-bold text-slate-100 p-6">Register 
 today & start your very own futureproofME campaign.</h1>
 <p className='ml-10 mr-10 text-white-100 text-left'><button type="button" 
 className=" py-2 text-sm font-bold p-4 m-4 rounded-full text-slate-100 
